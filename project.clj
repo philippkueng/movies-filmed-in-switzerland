99ef1e6e2c25f2d74de4c4f816d8d1750e1d159b
Philipp Küng <git@philippkueng.ch>
2015-03-25 22:51:54 +0100

    add a generate-url function and url-encode helpers

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "1.0.1"]
-                 [cheshire "5.4.0"]])
+                 [cheshire "5.4.0"]
+                 [com.cemerick/url "0.1.1"]])
--- src/movies_in_switzerland/core.clj
+++ src/movies_in_switzerland/core.clj
@@ -1,25 +1,38 @@
 (ns movies-in-switzerland.core
   (:require [clojure.string :as str]
             [clj-http.client :as client]
-            [cheshire.core :refer :all]))
+            [cheshire.core :refer :all]
+            [cemerick.url :refer (url url-encode)]))
 
 ;; read the locations.list file and count the lines
-(def raw-movie-lines
-  (with-open [rdr (clojure.java.io/reader
-                   "locations_list_switzerland_without_headers")]
-    (doall
-     (take 5 (line-seq rdr)))))
-
 (defn split-movie-location-line
   [line]
   "Splits a line entry with a movie, year, optional series info and location"
   (remove #(= "" %)
           (str/split line #"[\)]?\t|\"[\s]?[\(]?|\)\s\{|\}")))
 
+(def raw-movie-lines
+  (with-open [rdr (clojure.java.io/reader
+                   "locations_list_switzerland_without_headers")]
+    (doall
+     (take 5 (line-seq rdr)))))
+
+
+;; make a location request against yahoo's yql service
+(defn generate-url
+  [location]
+  "Given the location data generates the url to call"
+  (str "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text%3D%22"
+       (url-encode location)
+       "%22&format=json&callback="))
+
 (defn fetch-geo-data
-  [location-str]
+  [location]
   "Fetch the location information from Yahoo"
-  (:Result (:results (:query (parse-string (:body (client/get "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text%3D%22sfo%22&format=json&callback=")) true)))))
+  (:Result (:results (:query
+                      (parse-string
+                       (:body (client/get (generate-url location)))
+                       true)))))
 
 
 ;; url encode the location data
