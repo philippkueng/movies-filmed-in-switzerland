99de7451f7e3615c59f9e88b6e23221b52a16798
Philipp Küng <git@philippkueng.ch>
2015-03-26 20:49:32 +0100

    formatting a locations line

--- src/movies_in_switzerland/core.clj
+++ src/movies_in_switzerland/core.clj
@@ -11,11 +11,29 @@
   (remove #(= "" %)
           (str/split line #"[\)]?\t|\"[\s]?[\(]?|\)\s\{|\}")))
 
+;; read the locations.list file and parse the lines
 (def raw-movie-lines
   (with-open [rdr (clojure.java.io/reader
                    "locations_list_switzerland_without_headers")]
     (doall
      (take 5 (line-seq rdr)))))
+
+(defn split-movie-location-line
+  [line]
+  "Splits a line entry with a movie, year, optional series info and location"
+  (remove #(= "" %)
+          (str/split line #"[\)]?\t|\"[\s]?[\(]?|\)\s\{|\}")))
+
+(defn format-movie
+  [line]
+  "Turns a line entry into a map"
+  (let [m (split-movie-location-line line)]
+    (merge {:name (nth m 0)
+            :year (nth m 1)}
+           (cond
+            (= (count m) 3) {:location (nth m 2)}
+            (= (count m) 4) {:location (nth m 3)
+                             :series   (nth m 2)}))))
 
 
 ;; make a location request against yahoo's yql service
