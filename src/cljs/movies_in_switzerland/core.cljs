24f51c5e6397cd63a00c05d614021df1157b0352
Philipp Küng <git@philippkueng.ch>
2015-07-24 22:38:10 +0200

    adding ajax.core to load the json from the server and display it in the browser using hiccup

--- src/cljs/movies_in_switzerland/core.cljs
+++ src/cljs/movies_in_switzerland/core.cljs
@@ -1,6 +1,6 @@
 (ns ^:figwheel-always movies-in-switzerland.core
-    (:require
-              [reagent.core :as reagent :refer [atom]]))
+    (:require [reagent.core :as reagent :refer [atom]]
+              [ajax.core :refer [GET]]))
 
 (enable-console-print!)
 
@@ -8,10 +8,32 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Hello world to you!"}))
+(defonce movies (atom nil))
+
+(defn load-movies
+  "Fetches the movies data and parses it then assignes it to the movies atom"
+  []
+  (GET "/movie-year-location.json"
+       {:response-format :json
+        :keywords? true
+        :handler (fn [response]
+                   (reset! movies response))}))
 
 (defn hello-world []
-  [:h1 (:text @app-state)])
+  [:div.ui.active
+   (if (nil? @movies)
+     (list      
+      [:h1.ui.header.centered (:text @app-state)]
+      [:br]
+      [:div.ui.active.inline.medium.text.loader.centered "Loading assets..."])
+     (list
+      [:p (str "Loaded "
+               (count @movies)
+               " movies.")]
+      [:ul
+       (for [movie @movies]
+         [:li (:name movie)])]))])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
